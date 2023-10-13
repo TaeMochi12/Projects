59b6624b37d2cd0f6d25fdc9dd596df759a1d042
TaeMochi12 <himanshi4821@gmail.com>
2023-10-13 23:01:46 +0530

    Done with the webiste

--- html_css/Birthday_website/cake.js
+++ html_css/Birthday_website/cake.js
@@ -4,6 +4,6 @@ $(document).ready(function () {
         $(".flame").animate({ "opacity": 0 }, "fast");
         $(".flame2").animate({ "opacity": 0 }, "fast");
         $(".flame3").animate({ "opacity": 0 }, "fast");
-        $(".text").animate({ "top": -90, "opacity": 1 }, "fast");
+        $(".text").animate({ "top": -20, "opacity": 1 }, "fast");
     });
 });
